51a6f14e521e68deaeaef0d71d498acfea37048a
Julik Tarkhanov <me@julik.nl>
2025-02-15 14:04:36 +0100

    Store IV with ciphertext in CBC mode

--- lib/block_cipher_kit/aes_256_cbc_scheme.rb
+++ lib/block_cipher_kit/aes_256_cbc_scheme.rb
@@ -1,61 +1,49 @@
+require "securerandom"
+
 class BlockCipherKit::AES256CBCScheme < BlockCipherKit::BaseScheme
-  def initialize(encryption_key)
+  IV_LENGTH = 16
+
+  def initialize(encryption_key, iv_generator: SecureRandom)
     raise ArgumentError, "#{required_encryption_key_length} bytes of key material needed, at the minimum" unless encryption_key.bytesize >= required_encryption_key_length
-    @iv = BlockCipherKit::KeyMaterial.new(encryption_key.byteslice(0, 16))
-    @key = BlockCipherKit::KeyMaterial.new(encryption_key.byteslice(16, 32))
+    @iv_generator = iv_generator
+    @key = BlockCipherKit::KeyMaterial.new(encryption_key.byteslice(0, 32))
   end
 
   def required_encryption_key_length
-    48
+    32
   end
 
   def streaming_decrypt(from_ciphertext_io:, into_plaintext_io: nil, &blk)
     cipher = OpenSSL::Cipher.new("aes-256-cbc")
     cipher.decrypt
-    cipher.iv = @iv
+    cipher.iv = from_ciphertext_io.read(IV_LENGTH)
     cipher.key = @key
     read_copy_stream_via_cipher(source_io: from_ciphertext_io, cipher: cipher, destination_io: into_plaintext_io, &blk)
   end
 
   def streaming_encrypt(into_ciphertext_io:, from_plaintext_io: nil, &blk)
+    random_iv = @iv_generator.bytes(IV_LENGTH)
     cipher = OpenSSL::Cipher.new("aes-256-cbc")
     cipher.encrypt
-    cipher.iv = @iv
+    cipher.iv = random_iv
     cipher.key = @key
+    into_ciphertext_io.write(random_iv)
     write_copy_stream_via_cipher(source_io: from_plaintext_io, cipher: cipher, destination_io: into_ciphertext_io, &blk)
   end
 
   def streaming_decrypt_range(from_ciphertext_io:, range:, into_plaintext_io: nil, &blk)
     block_size = 16
-
     n_bytes_to_decrypt = range.end - range.begin + 1
     n_blocks_to_skip, offset_into_first_block = range.begin.divmod(block_size)
-
-    # If the from_io also contains some kind of header, we assume
-    # that the pointer has been moved to where ciphertext begins - i.e.
-    # using IO#seek. We need that pointer position so that we can
-    # seek to block offsets correctly - otherwise we need a wrapper
-    # which recomputes offsets in the IO
-    ciphertext_starts_at = from_ciphertext_io.pos
 
     cipher = OpenSSL::Cipher.new("aes-256-cbc")
     cipher.decrypt
     cipher.key = @key
 
-    # If the first block we will be reading is going to be block 0
-    # we can use our IV as-is
-    if n_blocks_to_skip.zero?
-      cipher.iv = @iv
-    else
-      # If we will be skipping blocks, we need the last skipped block
-      # as it gets used as the IV for the first block we will actually decrypt.
-      offset_of_preceding_block = (n_blocks_to_skip - 1) * block_size
-      from_ciphertext_io.seek(ciphertext_starts_at + offset_of_preceding_block)
-      cipher.iv = from_ciphertext_io.read(block_size)
-    end
-
-    # We need to read the blocks until the IO runs out, and we need to start reading at a block boundary
-    from_ciphertext_io.seek(ciphertext_starts_at + (block_size * n_blocks_to_skip))
+    # We need to read the IV either from the start of the IO (the initial IV)
+    # or from the block preceding the first block we need to decrypt
+    from_ciphertext_io.seek(from_ciphertext_io.pos + (n_blocks_to_skip * block_size))
+    cipher.iv = from_ciphertext_io.read(IV_LENGTH)
 
     writable = BlockCipherKit::BlockWritable.new(into_plaintext_io, &blk)
     lens_range = offset_into_first_block...(offset_into_first_block + n_bytes_to_decrypt)
@@ -63,6 +51,6 @@ class BlockCipherKit::AES256CBCScheme < BlockCipherKit::BaseScheme
 
     # TODO: it seems that if we read only the blocks we touch, we need to call cipher.final to get all the output - the cipher buffers,
     # but if we call .final without having read the entire ciphertext the cipher will barf. This needs to be fixed as it is certainly possible with CBC.
-    read_copy_stream_via_cipher(source_io: from_ciphertext_io, destination_io: lens, cipher: cipher, finalize_cipher: true, read_limit: from_ciphertext_io.size - ciphertext_starts_at)
+    read_copy_stream_via_cipher(source_io: from_ciphertext_io, destination_io: lens, cipher: cipher, finalize_cipher: true, read_limit: from_ciphertext_io.size - from_ciphertext_io.pos)
   end
 end
